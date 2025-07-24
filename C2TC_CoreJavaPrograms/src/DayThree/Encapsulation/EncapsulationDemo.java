8da3ba253d01193437dd4a77b242ba8abb40389e
INIYASHRE <INIYASHRE@DESKTOP-A7KDVV5>
2025-07-24 10:00:18 +0530

    Day 3

---
+++ C2TC_CoreJavaPrograms/src/DayThree/Encapsulation/EncapsulationDemo.java
@@ -0,0 +1,13 @@
+package DayThree.Encapsulation;
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+		
+		//object creation 
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(333);
+		obj.setName("Sharanya");
+		obj.setAge(20);
+		System.out.println(obj);
+	}
+}
