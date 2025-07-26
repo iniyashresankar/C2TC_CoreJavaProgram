d12ad1dce9a0504f8cbf7f9ca183d9f64fe1d159
INIYASHRE <INIYASHRE@DESKTOP-A7KDVV5>
2025-07-26 16:49:29 +0530

    Assignment 3

---
+++ C2TC_CoreJavaPrograms/src/Assignment3/Bank.java
@@ -0,0 +1,13 @@
+package Assignment3;
+
+public class Bank {
+	private static int totalAccounts = 0;
+
+    public static int getTotalAccounts() {
+        return totalAccounts;
+    }
+
+    public static void incrementAccounts() {
+        totalAccounts++;
+    }
+}
---
+++ C2TC_CoreJavaPrograms/src/Assignment3/SavingsAccount.java
@@ -0,0 +1,26 @@
+package Assignment3;
+
+public class SavingsAccount extends Account{
+	public SavingsAccount(String accountHolder, double initialBalance) {
+        super(accountHolder, initialBalance);
+    }
+
+    @Override
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+}
